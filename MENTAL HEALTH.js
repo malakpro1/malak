e53e5973e70b2e4a59db9b27cfc33b3616156818
malakpro1 <154622568+malakpro1@users.noreply.github.com>
2023-12-22 22:35:44 +0300

    Add files via upload

---
+++ MENTAL HEALTH.js
@@ -0,0 +1,7 @@
+function goody1()  {
+    alert(" The most important treatmment for mental health illness !");
+   document.getElementById("goody")
+   .style.display="block";
+   
+}
+setTimeout(goody1, 8000);
